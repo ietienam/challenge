489b0cfef4a9d25fbce4bc36d5adc293b36237c7
Ini Etienam <ietienam@gmail.com>
2021-01-08 22:46:03 +0100

    works

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const app = express();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-
+function streamToString(stream, cb) {
+  const chunks = [];
+  stream.on("data", (e) => {
+    chunks.push(e.toString());
+  });
+  stream.on("end", () => {
+    cb(chunks.join(""));
+  });
+}
 // https://expressjs.com/en/starter/basic-routing.html
 app.post("/", (request, response) => {
   let csv = { ...request.body };
@@ -20,141 +28,152 @@ app.post("/", (request, response) => {
       message: "Please provide input data in the right format",
     });
   } else {
-    let csvData;
-
-    let csvstream = requests(csv.url).pipe(fs.createWriteStream("data.csv"));
-    csvstream.on("finish", function () {
-      let instream = fs.createReadStream("data.csv");
-      let outstream = new stream();
-      let rl = readline.createInterface(instream, outstream);
-    });
-    csvstream.on("close", function () {
-      csvData = getCSVData(csv.url);
-      console.log("close");
-    });
-    csvstream.on("error", function (error) {
-      console.error(error);
-    });
-    const getCSVData = async (url) => {
-      let csvData;
-      let data = fs.readFile("./data.csv");
-      console.log(data);
-      csvData = data.split("\n");
-      if (csvData[csvData.length - 1] === "") {
-        csvData.pop();
-      }
-      //console.log(csvData);
-      return csvData;
-    };
-    const getHeaders = (csvData) => {
-      let headers = csvData[0];
-      headers = headers.replace("\r", "");
-      headers = headers.split(",");
-      return headers;
-    };
-    const csvHeaders = getHeaders(csvData);
-    //console.log(csvHeaders);
-
-    let csvRawBody = csvData.slice(1);
-    csvRawBody = csvRawBody.map((csv) => csv.replace("\r", ""));
-    //console.log(csvRawBody);
-    const getBody = (csvRawBody) => {
-      let data = [];
-      function operate(csvRawBody) {
-        if (csvRawBody.length === 0) return;
-        let value = csvRawBody[0].split(",").join(" ");
-        data.push(value);
-        operate(csvRawBody.slice(1));
-      }
-      operate(csvRawBody);
-      return data;
-    };
-    const csvBody = getBody(csvRawBody);
-    //console.log(csvBody);
-
-    const validateCSV = (csvBody, csvHeadersLength) => {
-      if (csvBody.length === 0) return true;
-      //console.log(csvBody[0].split(' ').length, csvHeadersLength, csvBody.slice(1))
-      if (csvBody[0].split(" ").length !== csvHeadersLength) return false;
-      return validateCSV(csvBody.slice(1), csvHeadersLength);
-    };
-    const isValid = validateCSV(csvBody, csvHeaders.length);
-    //if (!isValid) return "Invalid CSV File Format!";
-    //console.log(isValid);
-    if (!isValid) {
-      response.status(400).json({
-        message: "Invalid CSV file format",
-      });
-    } else {
-      const generateJSONObjectInArray = (csvHeaders, csvBody, fields) => {
-        let response = [];
-        let returnData;
-        if (fields == undefined || fields == null) {
-          function createObject(csvHeaders, csvBody) {
-            //console.log(csvBody.length)
-            if (csvBody.length === 0) return;
-            let data = {};
-            let arr = csvBody[0].split(" ");
-            //console.log(arr);
-            for (let i = 0; i < arr.length; i++) {
-              data[csvHeaders[i]] = arr[i];
-            }
-            response.push(data);
-            data = {};
-            arr = undefined;
-            createObject(csvHeaders, csvBody.slice(1));
-          }
-          createObject(csvHeaders, csvBody);
-          returnData = {
-            conversion_key: crypto.randomBytes(32).toString("hex"),
-            json: response,
-          };
-          return returnData;
-        } else {
-          function createTailoredObject(csvHeaders, csvBody, fields) {
-            if (csvBody.length === 0) return;
-            let data = {};
-            let fieldData = fields.map((val) => {
-              if (csvHeaders.includes(val)) {
-                return val;
-              }
-            });
-            //console.log(fieldData);
-            let arr = csvBody[0].split(" ");
-            for (let i = 0; i < arr.length; i++) {
-              data[csvHeaders[i]] = arr[i];
-              //data.fieldData[i] = csvBody[0][csvHeaders.indexOf(fieldData[i])];
-            }
-            for (let property in data) {
-              if (fieldData.includes(property) === false) {
-                delete data[property];
-              }
-            }
-            response.push(data);
-            data = {};
-            arr = undefined;
-            createTailoredObject(csvHeaders, csvBody.slice(1), fields);
-          }
-          createTailoredObject(csvHeaders, csvBody, fields);
-          returnData = {
-            conversion_key: crypto.randomBytes(32).toString("hex"),
-            json: response,
-          };
-          return returnData;
-        }
-      };
-      if (csv.select_fields) {
-        const csvJSON = JSON.stringify(
-          generateJSONObjectInArray(csvHeaders, csvBody, csv.select_fields)
-        );
-        response.status(200).send(csvJSON);
-      } else {
-        const csvJSON = JSON.stringify(
-          generateJSONObjectInArray(csvHeaders, csvBody)
-        );
-        response.status(200).send(csvJSON);
-      }
+    function downloadCSV(url, path) {
+      return requests(csv.url).pipe(fs.createWriteStream(path));
     }
+
+    const getCSVData = (url) => {
+      new Promise((resolve, reject) => {
+        downloadCSV(url, "data.csv")
+          .on("error", (err) => {
+            console.log(err);
+            response.json({
+              message: "Unable to download CSV from URL",
+            });
+            reject(`Unable to download csv`);
+          })
+          .on("finish", function () {
+            let csvData;
+            let data = fs.readFileSync("./data.csv", { encoding: "utf-8" });
+            //console.log(data);
+            csvData = data.split("\n");
+            if (csvData[csvData.length - 1] === "") {
+              csvData.pop();
+            }
+
+            const getHeaders = (csvData) => {
+              let headers = csvData[0];
+              headers = headers.replace("\r", "");
+              headers = headers.split(",");
+              return headers;
+            };
+            const csvHeaders = getHeaders(csvData);
+            console.log(csvHeaders);
+
+            let csvRawBody = csvData.slice(1);
+            csvRawBody = csvRawBody.map((csv) => csv.replace("\r", ""));
+            //console.log(csvRawBody);
+            const getBody = (csvRawBody) => {
+              let data = [];
+              function operate(csvRawBody) {
+                if (csvRawBody.length === 0) return;
+                let value = csvRawBody[0].split(",").join(" ");
+                data.push(value);
+                operate(csvRawBody.slice(1));
+              }
+              operate(csvRawBody);
+              return data;
+            };
+            const csvBody = getBody(csvRawBody);
+            //console.log(csvBody);
+
+            const validateCSV = (csvBody, csvHeadersLength) => {
+              if (csvBody.length === 0) return true;
+              //console.log(csvBody[0].split(' ').length, csvHeadersLength, csvBody.slice(1))
+              if (csvBody[0].split(" ").length !== csvHeadersLength)
+                return false;
+              return validateCSV(csvBody.slice(1), csvHeadersLength);
+            };
+            const isValid = validateCSV(csvBody, csvHeaders.length);
+            //if (!isValid) return "Invalid CSV File Format!";
+            console.log(isValid);
+            if (!isValid) {
+              response.status(400).json({
+                message: "Invalid CSV file format",
+              });
+            } else {
+              const generateJSONObjectInArray = (
+                csvHeaders,
+                csvBody,
+                fields
+              ) => {
+                let response = [];
+                let returnData;
+                if (fields == undefined || fields == null) {
+                  function createObject(csvHeaders, csvBody) {
+                    //console.log(csvBody.length)
+                    if (csvBody.length === 0) return;
+                    let data = {};
+                    let arr = csvBody[0].split(" ");
+                    //console.log(arr);
+                    for (let i = 0; i < arr.length; i++) {
+                      data[csvHeaders[i]] = arr[i];
+                    }
+                    response.push(data);
+                    data = {};
+                    arr = undefined;
+                    createObject(csvHeaders, csvBody.slice(1));
+                  }
+                  createObject(csvHeaders, csvBody);
+                  returnData = {
+                    conversion_key: crypto.randomBytes(32).toString("hex"),
+                    json: response,
+                  };
+                  return returnData;
+                } else {
+                  function createTailoredObject(csvHeaders, csvBody, fields) {
+                    if (csvBody.length === 0) return;
+                    let data = {};
+                    let fieldData = fields.map((val) => {
+                      if (csvHeaders.includes(val)) {
+                        return val;
+                      }
+                    });
+                    //console.log(fieldData);
+                    let arr = csvBody[0].split(" ");
+                    for (let i = 0; i < arr.length; i++) {
+                      data[csvHeaders[i]] = arr[i];
+                      //data.fieldData[i] = csvBody[0][csvHeaders.indexOf(fieldData[i])];
+                    }
+                    for (let property in data) {
+                      if (fieldData.includes(property) === false) {
+                        delete data[property];
+                      }
+                    }
+                    response.push(data);
+                    data = {};
+                    arr = undefined;
+                    createTailoredObject(csvHeaders, csvBody.slice(1), fields);
+                  }
+                  createTailoredObject(csvHeaders, csvBody, fields);
+                  returnData = {
+                    conversion_key: crypto.randomBytes(32).toString("hex"),
+                    json: response,
+                  };
+                  return returnData;
+                }
+              };
+              if (csv.select_fields) {
+                const csvJSON = JSON.stringify(
+                  generateJSONObjectInArray(
+                    csvHeaders,
+                    csvBody,
+                    csv.select_fields
+                  )
+                );
+                response.status(200).send(csvJSON);
+              } else {
+                const csvJSON = JSON.stringify(
+                  generateJSONObjectInArray(csvHeaders, csvBody)
+                );
+                response.status(200).send(csvJSON);
+              }
+            }
+            resolve(csvData);
+          });
+      });
+    };
+    getCSVData(csv.url);
   }
 });
 
